ec95a61908e1d51bc84f72f20c02369a4d8e7b81
ChristianChipman <cchip93@gmail.com>
2018-02-02 13:51:18 -0700

    2/2/2018 edit game and player classes

--- delirium/src/byui/cit260/DeliriumProject/model/Player.java
+++ delirium/src/byui/cit260/DeliriumProject/model/Player.java
@@ -28,7 +28,6 @@ public class Player implements Serializable {
         this.games = games;
     }
     
-
     public String getName() {
         return name;
     }
@@ -66,6 +65,5 @@ public class Player implements Serializable {
     public String toString() {
         return "Player{" + "name=" + name + '}';
     }
-    
-    
+
 }
