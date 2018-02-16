18bf2ae55ed7a81d7358e85312aedd94e7f7b50e
cradke <35538854+cradke@users.noreply.github.com>
2018-02-16 14:38:02 -0700

    2/16/2018 update for actor.java and puzzlecontrol.java

--- delirium/src/byui/cit260/delirium/control/PuzzleControl.java
+++ delirium/src/byui/cit260/delirium/control/PuzzleControl.java
@@ -13,30 +13,47 @@ import byui.cit260.DeliriumProject.model.Actor;
  */
 public class PuzzleControl {
 
-    public static void solvePuzzleVoodooBox(int boxB, int boxC, int boxD, int boxF, int boxG, int boxH, Actor actor) {
+    public static int solvePuzzleVoodooBox(int boxB, int boxC, int boxD,
+            int boxF, int boxG, int boxH, Actor actor) {
 
-        boolean door = false;
+        // Constant Variables
         int boxA = 2;
         int boxE = 5;
         int boxI = 8;
+        int insane;
 
-        if (boxA + boxB + boxC == 15
-                && boxD + boxE + boxF == 15
-                && boxG + boxH + boxI == 15
-                && boxA + boxD + boxG == 15
-                && boxB + boxE + boxH == 15
-                && boxC + boxF + boxI == 15
-                && boxA + boxE + boxI == 15
-                && boxG + boxE + boxC == 15) {
-
-            System.out.println("The voodoo box is correct and has been solved. The door unlocks and you can proceed");
-            door = true;
+        // if actor is null then return error
+        if (actor == null) {
+            return -1;
         } else {
-            System.out.println("That is incorrect. Try again. You feel a strange feeling in your head.");
-            int insane = actor.getInsanityLevel();
-            insane += 10;
-            actor.setInsanityLevel(insane);
-        }
 
+            // Check for negative numbers
+            if (boxB < 1 || boxC < 1 || boxD < 1 || boxF < 1 || boxF < 1 || boxG < 1 || boxH < 1) {
+                insane = actor.getInsanityLevel();
+                insane += 10;
+                actor.setInsanityLevel(insane);
+                return 0;
+            } else {
+
+                // Invalid inputs aren't checked for because they're equally punishable to
+                // incorrect inputs
+                if (boxA + boxB + boxC == 15
+                        && boxD + boxE + boxF == 15
+                        && boxG + boxH + boxI == 15
+                        && boxA + boxD + boxG == 15
+                        && boxB + boxE + boxH == 15
+                        && boxC + boxF + boxI == 15
+                        && boxA + boxE + boxI == 15
+                        && boxG + boxE + boxC == 15) {
+                    return 1;
+                } else {
+                    insane = actor.getInsanityLevel();
+                    insane += 10;
+                    actor.setInsanityLevel(insane);
+                    return 0;
+                }
+            }
+
+        }
     }
 }
