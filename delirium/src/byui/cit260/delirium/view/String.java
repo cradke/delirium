4bcc73442c79b6cf48a01534a98070d0df787505
brownjl9 <35307446+brownjl9@users.noreply.github.com>
2018-02-23 13:56:36 -0700

    2/5/18 Josh Personal Edit4

---
+++ delirium/src/byui/cit260/delirium/view/String.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.delirium.view;
+
+/**
+ *
+ * @author Joshua Brown
+ */
+public class String {
+    String playerName = new String();
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
