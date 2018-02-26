d7f3e27278a69665f441c7b1c6c94405ecfde0ed
brownjl9 <35307446+brownjl9@users.noreply.github.com>
2018-02-26 16:30:27 -0700

    2/26/18 Josh Edit

---
+++ delirium/src/byui/cit260/delirium/view/StartProgramView.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.delirium.view;
+
+/**
+ *
+ * @author Joshua Brown
+ */
+public class StartProgramView {
+    
+    public StartProgramView() {
+    
+    }
+    
+            
+    public void displayStartProgramView() {
+       boolean endView = false;
+        do {
+           String[] inputs = this.getInputs();
+            if (inputs == null){
+                return;
+            }
+            endView = doAction(inputs);
+        } while (endView != true);
+    }
+
+    private String[] getInputs() {
+        System.out.println("**** getInputs() called ***");
+        
+        String[] inputs = new String[1];
+        inputs[0] = "testInput";
+        
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+        System.out.println("**** doAction() called ***");
+        System.out.println("\tinputs = " + inputs[0]);
+        
+        return true;
+    }
+} 
