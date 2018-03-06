b302f71b2bc2cafdbad64ad2672f404ac019e344
ChristianChipman <cchip93@gmail.com>
2018-03-06 13:15:26 -0700

    2/2/2018 Added library

---
+++ delirium/src/byui/cit260/delirium/view/InventoryItemsView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.delirium.view;
+
+
+import byui.cit260.DeliriumProject.model.Player;
+import byui.cit260.delirium.control.GameControl;
+/**
+ *
+ * @author Christian
+ */
+public class InventoryItemsView {
+    
+}
