0a2461ca69f087d3ad36e5dcb9e361cf5299b84d
ChristianChipman <cchip93@gmail.com>
2018-03-07 15:51:29 -0700

    2/2/2018 Added library

---
+++ delirium/src/byui/cit260/delirium/control/ProgramControl.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.delirium.control;
+
+import byui.cit260.DeliriumProject.model.Player;
+
+/**
+ *
+ * @author Christian
+ */
+public class ProgramControl {
+
+    public static void createNewGame(Player player) {
+        System.out.println("*** Create new Game called ***");
+    }
+    
+}
