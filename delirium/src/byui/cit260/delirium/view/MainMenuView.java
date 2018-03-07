40382e010b98e3b5d89ba0dd23355709f9253a2a
cradke <35538854+cradke@users.noreply.github.com>
2018-03-07 13:48:23 -0700

    3/7/1018 Carl's Edit

--- delirium/src/byui/cit260/delirium/view/MainMenuView.java
+++ delirium/src/byui/cit260/delirium/view/MainMenuView.java
@@ -5,11 +5,16 @@
  */
 package byui.cit260.delirium.view;
 
+import java.util.*;
+
 /**
  *
  * @author Joshua Brown
  */
 public class MainMenuView {
+
+    private String description;
+    private String promptMessage;
 
     public MainMenuView() {
     }
@@ -27,28 +32,60 @@ public class MainMenuView {
         }
 
     private String[] getInputs() {
-        return null;
         
-  //       inputs = new String array whose length = no. of inputs
- //Display the instructions
-
-//valid = false
-//WHILE valid == false (while input value is not valid)
-
-//Display the prompt message
-//Get the value entered from the keyboard
-//Trim off leading and trailing blanks from the value
-//IF length of the value < 1 then
-//Display "You must enter a value.”
-//Continue
-//ENDIF
-//Assign value to the next position in the inputs array
-//valid = true
-//ENDWHILE
-
-//RETURN inputs 
+        String[] inputs = new String[3];
+        System.out.println("DELIRIUM - Main Menu ");
+        
+        // The 'q' key boi
+        this.description = "Press 'Q' to quit the view Press 'N' for new game Press 'L' to load a saved game";
+        this.promptMessage = "Enter in a command! ('N', 'L' or 'Q')";
+        inputs[0] = this.getInput();
+        if (inputs[0].toUpperCase().equals("Q")) {
+            return inputs;
+        }
+        
+        // The 'l' key boi
+        this.description = "Press 'Q' to quit the view Press 'N' for new game Press 'L' to load a saved game";
+        this.promptMessage = "Enter in a command! ('N', 'L' or 'Q')";
+        inputs[1] = this.getInput();
+        if (inputs[1].toUpperCase().equals("L")) {
+            return inputs;
+        }
+        
+        // The 'n' key boi
+        this.description = "Press 'Q' to quit the view Press 'N' for new game Press 'L' to load a saved game";
+        this.promptMessage = "Enter in a command! ('N', 'L' or 'Q')";
+        inputs[2] = this.getInput();
+        if (inputs[2].toUpperCase().equals("N")) {
+            return inputs;
+        }
+        
+        return inputs;
 }
 
+    private String getInput() {
+        Scanner inFile;
+        inFile = new Scanner(System.in);
+        boolean valid = false;
+        String input = null;
+        
+        while (valid == false) {
+            System.out.println(this.description);
+            System.out.println(this.promptMessage);
+            input = inFile.nextLine();
+            input = input.trim();
+            
+            if (input.length() < 1) {
+                System.out.println("Please enter in a correct value");
+                valid = false;
+            } else {
+                valid = true; 
+            }
+            
+        }
+        return input;
+    }
+    
     private boolean doAction(String[] inputs) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }   
